73b62c6deeffd1569a47d034af8cca2589468873
SuperAuguste <auguste.rame@gmail.com>
2019-09-21 09:45:12 -0400

    Changes baby, hell yeah!

--- src/index.js
+++ src/index.js
@@ -38,8 +38,9 @@ class Database {
 
 		}
 
-		this._docIndMap = new Map();
+		// this._docIndMap = new Map();
 		this._documents = [];
+		this._documentIdentifiers = new Set();
 
 		this._streams.write.setMaxListeners(100);
 		this.idInstance = hyperid({
@@ -56,14 +57,47 @@ class Database {
 
 	}
 
-	async insert (document) {
+	get (index) {
+
+		return this._documents[index];
+
+	}
+
+	insert (document) {
 
 		this._queues.write.push(document);
 		return this._fulfillWrite();
 
 	}
 
-	async insertBulk (bulk) {
+	edit (id, document) {
+
+		return this._fulfillWrite(id, document);
+
+	}
+
+	delete (id) {
+
+		return new Promise((resolve, reject) => {
+
+			const index = this.findIndex(_ => _._id === id);
+
+			this._documents.splice(index, 1);
+			this._documentIdentifiers.delete(id);
+
+			this._streams.write.write(id + "-DELETE" + "\n", err => {
+
+				if (err) throw err;
+
+				resolve();
+				
+			});
+
+		});
+
+	}
+
+	insertBulk (bulk) {
 
 		const promises = [];
 		for (const document of bulk) {
@@ -89,6 +123,12 @@ class Database {
 
 	}
 
+	findById (id) {
+
+		return this.findOne(_ => _._id === id);
+
+	}
+
 	findIndex (fn) {
 
 		return this._documents.findIndex(fn);
@@ -97,26 +137,35 @@ class Database {
 
 	findIndexes (fn) {
 
-		return this.find(fn).map(_ => this._docIndMap.get(_._id));
+		return this._documents.map((e, i) => fn(e) ? i : undefined).filter(_ => _);
 
 	}
 
-	async _push (id, data) {
+	_push (id, document) {
 
-		if (this._docIndMap.has(id)) {
+		if (this._documentIdentifiers.has(id)) {
 
-			this._documents.splice(this._docIndMap.get(id), 1);
+			let index = this.findIndex(_ => _._id === id);
+
+			this._documents[index] = {
+				
+				...document,
+				_id: id
+
+			}
+
+		} else {
+			
+			this._documents.push({
+						
+				...document,
+				_id: id
+				
+			});
+
+			this._documentIdentifiers.add(id);
 
 		}
-
-		this._docIndMap.set(id, this._documents.length);
-
-		this._documents.push({
-						
-			...data,
-			_id: id
-			
-		});
 
 	}
 
@@ -130,7 +179,21 @@ class Database {
 		
 				if (!chunk.trim()) return;
 
-				this._push(chunk.slice(0, 33).toString(), JSON.parse(chunk.slice(34).trim(), _jsonParse));
+				const id = chunk.slice(0, 33).toString();
+				const data = chunk.slice(34).trim();
+
+				if (data === "DELETE") {
+
+					const index = this.findIndex(_ => _._id === id);
+
+					this._documents.splice(index, 1);
+					this._documentIdentifiers.delete(id);
+
+					return;
+
+				}
+
+				this._push(id, JSON.parse(data, _jsonParse));
 
 			});
 			readStream.on("end", () => resolve(this._documents));
@@ -141,11 +204,13 @@ class Database {
 
 	async _fulfillWrite (uid = this.idInstance(), data = this._queues.write.shift()) {
 
+		delete data._id;
+
 		return new Promise((resolve, reject) => {
 
 			const id = uid || this.idInstance();
 
-			this._documents.push({...data, _id: id});
+			this._push(id, data);
 
 			this._streams.write.write(id + "-" + JSON.stringify(data) + "\n", err => {
 
--- test/tests/read.js
+++ test/tests/read.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const chalk = require("chalk").default;
+const assert = require("assert");
 const Database = require("../../src");
 
 module.exports = {
@@ -34,6 +35,54 @@ module.exports = {
 		console.time("findIndex()");
 		db.findIndex(_ => _.value.startsWith("0.2")).length;
 		console.timeEnd("findIndex()");
+		console.log();
+
+		console.log(chalk.blue(`Testing "findById"`));
+
+		console.time("findById()");
+		assert(db.findById(db.get(0)._id), "findById() returned undefined!");
+		console.timeEnd("findById()");
+		console.log();
+
+		console.log(chalk.blue(`Testing "edit" and "delete"`));
+
+		const first = db.get(0);
+		const second = db.get(1);
+		const third = db.get(2);
+
+		console.time("edit()");
+
+		db.edit(first._id, {
+
+			...first,
+			modified: true
+
+		});
+
+		db.edit(second._id, {
+
+			...second,
+			modified2: true
+
+		});
+
+		db.edit(third._id, {
+
+			...third,
+			modified3: true
+
+		});
+
+		assert(db.get(0).modified && db.get(1).modified2 && db.get(2).modified3, "Items could not be edited!");
+
+		console.timeEnd("edit()");
+		console.time("delete()");
+
+		await db.delete(first._id);
+
+		assert(db.get(0)._id === second._id, "Item could not be deleted!");
+
+		console.timeEnd("delete()");
 		console.log();
 
 	}
